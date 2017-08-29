1e24457bb8ba2cc898f81b504049a28b4790a464
Michal Drbohlav <m.drbohlav1@gmail.com>
2017-08-29 15:46:40 +0200

    ability to display multiple numberpickers in one dialog

--- android/src/main/java/fr/bamlab/reactnativenumberpickerdialog/RNNumberPickerDialogModule.java
+++ android/src/main/java/fr/bamlab/reactnativenumberpickerdialog/RNNumberPickerDialogModule.java
@@ -3,9 +3,13 @@ package fr.bamlab.reactnativenumberpickerdialog;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -15,6 +19,7 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableType;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,19 +42,123 @@ class RNNumberPickerDialogModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void show(final ReadableMap options, final Callback onSuccess, final Callback onFailure) {
-        ReadableArray values = options.getArray("values");
-        if (values.size() == 0) {
-            onFailure.invoke("values array must not be empty");
-            return;
+        LinearLayout llContainer = new LinearLayout(getCurrentActivity());
+        LinearLayout.LayoutParams llContainerParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        llContainer.setOrientation(LinearLayout.HORIZONTAL);
+        llContainer.setLayoutParams(llContainerParams);
+
+        ReadableArray columns = options.getArray("options");
+        final ArrayList<NumberPicker> pickers = new ArrayList<>();
+
+        for (int i = 0; i < columns.size(); i++) {
+            LinearLayout llColContainer = new LinearLayout((getCurrentActivity()));
+            LinearLayout.LayoutParams llColContainerParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1f
+            );
+            llColContainer.setLayoutParams(llColContainerParams);
+            llColContainer.setOrientation(LinearLayout.HORIZONTAL);
+            llColContainer.setGravity(Gravity.CENTER);
+
+            ReadableMap colData = columns.getMap(i);
+            ReadableArray colValues = colData.getArray("values");
+
+            if (colValues.size() == 0) {
+                onFailure.invoke("values array must not be empty");
+                return;
+            }
+
+            NumberPicker npCol = new NumberPicker(getCurrentActivity());
+            LinearLayout.LayoutParams npColParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            npCol.setLayoutParams(npColParams);
+
+            npCol.setMinValue(0);
+            npCol.setMaxValue(colValues.size() - 1);
+
+            String[] colDisplayeValues = getColDisplayedValues(colValues);
+
+            npCol.setDisplayedValues(colDisplayeValues);
+            npCol.setWrapSelectorWheel(false);
+            npCol.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+            if (colData.hasKey("selected")) {
+                final int selected = colData.getInt("selected");
+
+                if (selected >= colValues.size()) {
+                    onFailure.invoke("selected index is out of range");
+                    return;
+                }
+
+                npCol.setValue(selected);
+            } else {
+                npCol.setValue(0);
+            }
+
+            llColContainer.addView(npCol);
+            pickers.add(npCol);
+
+            if (colData.hasKey("label")) {
+                TextView tvColLabel = new TextView(getCurrentActivity());
+                LinearLayout.LayoutParams tvColLabelParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                tvColLabel.setLayoutParams(tvColLabelParams);
+
+                tvColLabel.setText(colData.getString("label"));
+
+                llColContainer.addView(tvColLabel);
+            }
+
+            llContainer.addView(llColContainer);
         }
 
-        final NumberPicker picker = new NumberPicker(getCurrentActivity());
-        picker.setMinValue(0);
-        picker.setMaxValue(values.size() - 1);
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(getCurrentActivity());
 
+        if (options.hasKey("title")) {
+            alertDialog.setTitle(options.getString("title"));
+        }
+
+        if (options.hasKey("message")) {
+            alertDialog.setMessage(options.getString("message"));
+        }
+
+        alertDialog.setView(llContainer);
+
+        if (options.hasKey("positiveButtonLabel")) {
+          alertDialog.setPositiveButton(options.getString("positiveButtonLabel"), new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog, int whichButton) {
+                  ArrayList<Integer> selected = new ArrayList<>();
+                  for (int i = 0; i < pickers.size(); i++) {
+                      selected.add(pickers.get(i).getValue());
+                  }
+                  onSuccess.invoke(selected.toString());
+              }
+          });
+        }
+
+        if (options.hasKey("negativeButtonLabel")) {
+            alertDialog.setNegativeButton(options.getString("negativeButtonLabel"), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    onSuccess.invoke(-1);
+                }
+            });
+        }
+
+        alertDialog.create().show();
+    }
+
+    private String[] getColDisplayedValues(ReadableArray values) {
         String[] displayedValues = new String[values.size()];
 
-        for(int i = 0; i < values.size(); ++i) {
+        for(int i = 0; i < values.size(); i++) {
             ReadableType indexType = values.getType(i);
             switch(indexType) {
                 case Boolean:
@@ -72,51 +181,6 @@ class RNNumberPickerDialogModule extends ReactContextBaseJavaModule {
             }
         }
 
-        picker.setDisplayedValues(displayedValues);
-        picker.setWrapSelectorWheel(false);
-        picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
-
-        if (options.hasKey("selected")) {
-            final int selected = options.getInt("selected");
-
-            if (selected >= values.size()) {
-                onFailure.invoke("selected index is out of range");
-                return;
-            }
-
-            picker.setValue(selected);
-        } else {
-            picker.setValue(0);
-        }
-
-        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(getCurrentActivity());
-
-        if (options.hasKey("title")) {
-          alertDialog.setTitle(options.getString("title"));
-        }
-
-        if (options.hasKey("message")) {
-          alertDialog.setMessage(options.getString("message"));
-        }
-
-        alertDialog.setView(picker);
-
-        if (options.hasKey("positiveButtonLabel")) {
-          alertDialog.setPositiveButton(options.getString("positiveButtonLabel"), new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int whichButton) {
-                  onSuccess.invoke(picker.getValue());
-              }
-          });
-        }
-
-        if (options.hasKey("negativeButtonLabel")) {
-          alertDialog.setNegativeButton(options.getString("negativeButtonLabel"), new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int whichButton) {
-                  onSuccess.invoke(-1);
-              }
-          });
-        }
-
-        alertDialog.create().show();
+        return displayedValues;
     }
 }
